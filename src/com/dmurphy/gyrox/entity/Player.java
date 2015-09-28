1993e39f187b935d46034f346e3deaf0c41f5730
Drew Murphy <dr3wmurphy@hotmail.com>
2015-09-28 16:12:18 -0500

    Fixed skybox rendering

--- src/com/dmurphy/gyrox/entity/Player.java
+++ src/com/dmurphy/gyrox/entity/Player.java
@@ -1,9 +1,7 @@
 package com.dmurphy.gyrox.entity;
 
 import java.util.ArrayList;
-
 import javax.microedition.khronos.opengles.GL10;
-
 import com.dmurphy.gyrox.game.GameState;
 import com.dmurphy.gyrox.model.Model;
 import com.dmurphy.gyrox.model.Vector;
@@ -13,242 +11,224 @@ import com.dmurphy.gyrox.world.Lighting;
 
 public class Player {
 
-	private Model ship;
-	private int shipColor;
+    private final Model ship;
+    private final int shipColor;
 
-	private int score;
-	private Vector position;
+    private int score;
+    private final Vector position;
 
-	private float speed;
-	public long turnTime;
+    private float speed;
+    public long turnTime;
 
-	private int direction;
-	private int prevDirection;
+    private int direction;
+    private int prevDirection;
 
-	private final float SPEED_OZ_FREQ = 1200.0f;
-	private final float SPEED_OZ_FACTOR = 0.09f;
+    private final float SPEED_OZ_FREQ = 1200.0f;
+    private final float SPEED_OZ_FACTOR = 0.09f;
 
-	private final float dirAngles[] = { 0.0f, -90.0f, -180.0f, 90.0f, 180.0f,
-			-270.0f };
-	public final int TURN_LEFT = 3;
-	public final int TURN_RIGHT = 1;
-	public final int TURN_LENGTH = 200;
+    private final float dirAngles[] = {0.0f, -90.0f, -180.0f, 90.0f, 180.0f, -270.0f};
+    public final int TURN_LEFT = 3;
+    public final int TURN_RIGHT = 1;
+    public final int TURN_LENGTH = 200;
 
-	public final float DIRS_X[] = { 0.0f, -1.0f, 0.0f, 1.0f };
-	public final float DIRS_Y[] = { -1.0f, 0.0f, 1.0f, 0.0f };
-	private final int LOD_DIST[][] = { { 1000, 1000, 1000 }, { 100, 200, 400 },
-			{ 30, 100, 200 }, { 10, 30, 150 } };
+    public final float DIRS_X[] = {0.0f, -1.0f, 0.0f, 1.0f};
+    public final float DIRS_Y[] = {-1.0f, 0.0f, 1.0f, 0.0f};
+    private final int LOD_DIST[][] = { {1000, 1000, 1000}, {100, 200, 400}, {30, 100, 200}, {10, 30, 150}};
 
-	public Player(float gridSize, Model mesh, HUD hud) {
-		speed = 10.0f;
-		ship = mesh;
-		score = 0;
+    public Player(float gridSize, Model mesh, HUD hud) {
+        speed = 10.0f;
+        ship = mesh;
+        score = 0;
 
-		shipColor = GameState.mPrefs.playerColorIndex();
+        shipColor = GameState.mPrefs.playerColorIndex();
 
-		position = new Vector(gridSize / 2, gridSize / 2, 2.0f);
-	}
+        position = new Vector(gridSize / 2, gridSize / 2, 2.0f);
+    }
 
-	public void doTurn(int direction, long time) {
-		prevDirection = this.direction;
-		this.direction = (this.direction + direction) % 4;
-		turnTime = time;
+    public void doTurn(int direction, long time) {
+        prevDirection = this.direction;
+        this.direction = (this.direction + direction) % 4;
+        turnTime = time;
 
-	}
+    }
 
-	public void doMovement(long timeDelta, long time) {
-		float fs;
-		float t;
+    public void doMovement(long timeDelta, long time) {
+        float fs;
+        float t;
 
-		if (speed > 0.0f) {
-			fs = (float) (1.0f - SPEED_OZ_FACTOR + SPEED_OZ_FACTOR
-					* Math.cos(0.0f * (float) Math.PI / 4.0f
-							+ (time % SPEED_OZ_FREQ) * 2.0f * Math.PI
-							/ SPEED_OZ_FREQ));
+        if (speed > 0.0f) {
+            fs = (float) (1.0f - SPEED_OZ_FACTOR + SPEED_OZ_FACTOR * Math.cos(0.0f * (float) Math.PI / 4.0f + (time % SPEED_OZ_FREQ) * 2.0f * Math.PI / SPEED_OZ_FREQ));
 
-			if (GameState.getBoostRemaining() > 0)
-				t = timeDelta / 100.0f * (speed * 3f) * fs;
-			else
-				t = timeDelta / 100.0f * speed * fs;
+            if (GameState.getBoostRemaining() > 0)
+                t = timeDelta / 100.0f * (speed * 3f) * fs;
+            else
+                t = timeDelta / 100.0f * speed * fs;
 
-			position.point[0] += t * DIRS_X[direction];
-			position.point[1] += t * DIRS_Y[direction];
+            position.point[0] += t * DIRS_X[direction];
+            position.point[1] += t * DIRS_Y[direction];
 
-			doCrashTestPickup(GameState.pickups, ship);
-			collideBoostTest(GameState.boost, ship);
-		}
-	}
+            doCrashTestPickup(GameState.pickups, ship);
+            collideBoostTest(GameState.boost, ship);
+        }
+    }
 
-	public void draw(GL10 gl, long time, long timeDelta, Lighting lights) {
-		gl.glPushMatrix();
-		gl.glTranslatef(getXCoord(), getYCoord(), 0.0f);
+    public void draw(GL10 gl, long time, long timeDelta, Lighting lights) {
+        gl.glPushMatrix();
+        gl.glTranslatef(getXCoord(), getYCoord(), 0.0f);
 
-		doShipRotation(gl, time);
-		gl.glEnable(GL10.GL_LIGHTING);
+        doShipRotation(gl, time);
+        gl.glEnable(GL10.GL_LIGHTING);
 
-		gl.glEnable(GL10.GL_DEPTH_TEST);
-		gl.glDepthMask(true);
-		gl.glEnable(GL10.GL_NORMALIZE);
-		gl.glTranslatef(0.0f, 0.0f, ship.getBBoxSize().point[2] / 2.0f);
-		gl.glEnable(GL10.GL_CULL_FACE);
-		ship.draw(gl, PlayerColor.specular[shipColor],
-				PlayerColor.diffuse[shipColor]);
-		gl.glDisable(GL10.GL_CULL_FACE);
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+        gl.glDepthMask(true);
+        gl.glEnable(GL10.GL_NORMALIZE);
+        gl.glTranslatef(0.0f, 0.0f, ship.getBBoxSize().point[2] / 2.0f);
+        gl.glEnable(GL10.GL_CULL_FACE);
+        ship.draw(gl, PlayerColor.specular[shipColor], PlayerColor.diffuse[shipColor]);
+        gl.glDisable(GL10.GL_CULL_FACE);
 
-		gl.glDisable(GL10.GL_BLEND);
-		gl.glDisable(GL10.GL_LIGHTING);
-		gl.glPopMatrix();
+        gl.glDisable(GL10.GL_BLEND);
+        gl.glDisable(GL10.GL_LIGHTING);
+        gl.glPopMatrix();
 
-	}
+    }
 
-	public boolean isVisible(Camera cam) {
-		Vector v1;
-		Vector v2;
-		Vector tmp = new Vector(getXCoord(), getYCoord(), 0.0f);
-		int levelOfDetail = 2;
-		float d, s;
-		int i;
-		int LC_LOD = 3;
-		float fov = 120;
+    public boolean isVisible(Camera cam) {
+        Vector v1;
+        Vector v2;
+        Vector tmp = new Vector(getXCoord(), getYCoord(), 0.0f);
+        int levelOfDetail = 2;
+        float d, s;
+        int i;
+        int LC_LOD = 3;
+        float fov = 120;
 
-		boolean retValue;
+        boolean retValue;
 
-		v1 = cam.getTarget().subtract(cam.getCam());
-		v1.normalize();
+        v1 = cam.getTarget().subtract(cam.getCam());
+        v1.normalize();
 
-		v2 = cam.getCam().subtract(tmp);
+        v2 = cam.getCam().subtract(tmp);
 
-		d = v2.length();
+        d = v2.length();
 
-		for (i = 0; i < LC_LOD && d >= LOD_DIST[levelOfDetail][i]; i++)
-			;
+        for (i = 0; i < LC_LOD && d >= LOD_DIST[levelOfDetail][i]; i++)
+            ;
 
-		if (i >= LC_LOD) {
-			retValue = false;
-		} else {
-			v2 = tmp.subtract(cam.getCam());
-			v2.normalize();
+        if (i >= LC_LOD) {
+            retValue = false;
+        } else {
+            v2 = tmp.subtract(cam.getCam());
+            v2.normalize();
 
-			s = v1.dot(v2);
-			d = (float) Math.cos((fov / 2) * 2 * Math.PI / 360.0f);
+            s = v1.dot(v2);
+            d = (float) Math.cos((fov / 2) * 2 * Math.PI / 360.0f);
 
-			if (s < d - (ship.getBBoxRadius() * 2.0f)) {
-				retValue = false;
-			} else {
-				retValue = true;
-			}
+            if (s < d - (ship.getBBoxRadius() * 2.0f)) {
+                retValue = false;
+            } else {
+                retValue = true;
+            }
 
-		}
+        }
 
-		return retValue;
-	}
+        return retValue;
+    }
 
-	private float getDirAngle(long time) {
-		int prevDirection;
-		float angleDirection;
+    private float getDirAngle(long time) {
+        int prevDirection;
+        float angleDirection;
 
-		if (time < TURN_LENGTH) {
-			prevDirection = this.prevDirection;
-			if (direction == 3 && prevDirection == 2) {
-				prevDirection = 4;
-			}
-			if (direction == 2 && prevDirection == 3) {
-				prevDirection = 5;
-			}
-			angleDirection = ((TURN_LENGTH - time) * dirAngles[prevDirection] + time
-					* dirAngles[direction])
-					/ TURN_LENGTH;
-		} else {
-			angleDirection = dirAngles[direction];
-		}
-		return angleDirection;
-	}
+        if (time < TURN_LENGTH) {
+            prevDirection = this.prevDirection;
+            if (direction == 3 && prevDirection == 2) {
+                prevDirection = 4;
+            }
+            if (direction == 2 && prevDirection == 3) {
+                prevDirection = 5;
+            }
+            angleDirection = ((TURN_LENGTH - time) * dirAngles[prevDirection] + time * dirAngles[direction]) / TURN_LENGTH;
+        } else {
+            angleDirection = dirAngles[direction];
+        }
+        return angleDirection;
+    }
 
-	private void doShipRotation(GL10 gl, long currentTime) {
-		long time = currentTime - turnTime;
-		float dirAngle;
-		float axis = 1.0f;
-		float angle;
+    private void doShipRotation(GL10 gl, long currentTime) {
+        long time = currentTime - turnTime;
+        float dirAngle;
+        float axis = 1.0f;
+        float angle;
 
-		dirAngle = getDirAngle(time);
+        dirAngle = getDirAngle(time);
 
-		gl.glRotatef(dirAngle, 0.0f, 0.0f, 1.0f);
+        gl.glRotatef(dirAngle, 0.0f, 0.0f, 1.0f);
 
-		if ((time < TURN_LENGTH) && (prevDirection != direction)) {
-			if ((direction < prevDirection) && (prevDirection != 3)) {
-				axis = -1.0f;
-			} else if (((prevDirection == 3) && (direction == 2))
-					|| ((prevDirection == 0) && (direction == 3))) {
-				axis = -1.0f;
-			}
-			angle = (float) Math.sin((Math.PI * time / TURN_LENGTH)) * 25.0f;
-			gl.glRotatef(angle, 0.0f, (axis * -1.0f), 0.0f);
-		}
-	}
+        if ((time < TURN_LENGTH) && (prevDirection != direction)) {
+            if ((direction < prevDirection) && (prevDirection != 3)) {
+                axis = -1.0f;
+            } else if (((prevDirection == 3) && (direction == 2)) || ((prevDirection == 0) && (direction == 3))) {
+                axis = -1.0f;
+            }
+            angle = (float) Math.sin((Math.PI * time / TURN_LENGTH)) * 25.0f;
+            gl.glRotatef(angle, 0.0f, (axis * -1.0f), 0.0f);
+        }
+    }
 
-	public float getXCoord() {
-		return position.point[0];
-	}
+    public float getXCoord() {
+        return position.point[0];
+    }
 
-	public float getYCoord() {
-		return position.point[1];
-	}
+    public float getYCoord() {
+        return position.point[1];
+    }
 
-	public float getSpeed() {
-		return speed;
-	}
+    public float getSpeed() {
+        return speed;
+    }
 
-	public void setSpeed(float sp) {
-		speed = sp;
-	}
+    public void setSpeed(float sp) {
+        speed = sp;
+    }
 
-	public int getDirection() {
-		return direction;
-	}
+    public int getDirection() {
+        return direction;
+    }
 
-	public int getLastDirection() {
-		return prevDirection;
-	}
+    public int getLastDirection() {
+        return prevDirection;
+    }
 
-	public void addScore(int val) {
-		score += val;
-	}
+    public void addScore(int val) {
+        score += val;
+    }
 
-	public int getScore() {
-		return score;
-	}
+    public int getScore() {
+        return score;
+    }
 
-	public void doCrashTestPickup(ArrayList<Pickup> pickups, Model mesh) {
-		for (int j = 0; j < pickups.size(); j++) {
-			float dist = (float) Math
-					.sqrt(Math.pow(this.getXCoord()
-							- pickups.get(j).getXCoord(), 2)
-							+ Math.pow(this.getYCoord()
-									- pickups.get(j).getYCoord(), 2));
+    public void doCrashTestPickup(ArrayList<Pickup> pickups, Model mesh) {
+        for (int j = 0; j < pickups.size(); j++) {
+            float dist = (float) Math.sqrt(Math.pow(this.getXCoord() - pickups.get(j).getXCoord(), 2) + Math.pow(this.getYCoord() - pickups.get(j).getYCoord(), 2));
 
-			if (dist <= (mesh.getBBoxRadius() + ((pickups.get(j).getModel()
-					.getBBoxRadius()*3)))) {
-				GameState.removePickup(j);
-				this.addScore(10 * GameState.getMultiplier());
-				GameState.playPickup();
-				GameState.incrementTime(GameState.PICKUP_TIME_INCREMENT
-						* GameState.DIMINISHING_RETURNS);
-				GameState.pickedUp();
-			}
-		}
-	}
+            if (dist <= (mesh.getBBoxRadius() + ((pickups.get(j).getModel().getBBoxRadius() * 3)))) {
+                GameState.removePickup(j);
+                this.addScore(10 * GameState.getMultiplier());
+                GameState.playPickup();
+                GameState.incrementTime(GameState.PICKUP_TIME_INCREMENT * GameState.DIMINISHING_RETURNS);
+                GameState.pickedUp();
+            }
+        }
+    }
 
-	public void collideBoostTest(ArrayList<SpeedBoost> boost, Model mesh) {
-		for (int j = 0; j < boost.size(); j++) {
-			float dist = (float) Math.sqrt(Math.pow(this.getXCoord()
-					- boost.get(j).getXpos(), 2)
-					+ Math.pow(this.getYCoord() - boost.get(j).getYpos(), 2));
+    public void collideBoostTest(ArrayList<SpeedBoost> boost, Model mesh) {
+        for (int j = 0; j < boost.size(); j++) {
+            float dist = (float) Math.sqrt(Math.pow(this.getXCoord() - boost.get(j).getXpos(), 2) + Math.pow(this.getYCoord() - boost.get(j).getYpos(), 2));
 
-			if (dist <= (mesh.getBBoxRadius() + (boost.get(j).getModel()
-					.getBBoxRadius() * 5f))) {
-				GameState.boost();
-				GameState.playBoost();
-			}
-		}
-	}
+            if (dist <= (mesh.getBBoxRadius() + (boost.get(j).getModel().getBBoxRadius() * 5f))) {
+                GameState.boost();
+                GameState.playBoost();
+            }
+        }
+    }
 }
--- src/com/dmurphy/gyrox/world/WorldGraphics.java
+++ src/com/dmurphy/gyrox/world/WorldGraphics.java
@@ -2,183 +2,187 @@ package com.dmurphy.gyrox.world;
 
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
-
 import javax.microedition.khronos.opengles.GL10;
-
 import android.content.Context;
-
 import com.dmurphy.gyrox.R;
+import com.dmurphy.gyrox.entity.Player;
 import com.dmurphy.gyrox.model.GLTexture;
 import com.dmurphy.gyrox.util.GraphicUtils;
 
 public class WorldGraphics {
 
-	float gridSize;
-	ByteBuffer indexBuffer;
-	FloatBuffer textureBuffer;
-	FloatBuffer floorTexBuffer;
-	FloatBuffer skyboxVertexBuffers[] = new FloatBuffer[6];
-	int nIndices;
+    float gridSize;
+    ByteBuffer indexBuffer;
+    FloatBuffer textureBuffer;
+    FloatBuffer floorTexBuffer;
+    FloatBuffer skyboxVertexBuffers[] = new FloatBuffer[6];
+    int nIndices;
+    Player player;
 
-	// Textures
-	GLTexture sbTopTexture;
-	GLTexture sbBottomTexture;
-	GLTexture sbLeftTexture;
-	GLTexture sbRightTexture;
-	GLTexture sbFrontTexture;
-	GLTexture sbBackTexture;
+    // Textures
+    GLTexture sbTopTexture;
+    GLTexture sbBottomTexture;
+    GLTexture sbLeftTexture;
+    GLTexture sbRightTexture;
+    GLTexture sbFrontTexture;
+    GLTexture sbBackTexture;
 
-	GLTexture floorTexture;
+    GLTexture floorTexture;
 
-	public WorldGraphics(GL10 gl, Context context, float gridSize) {
-		// Save Grid Size
-		this.gridSize = gridSize;
+    public WorldGraphics(GL10 gl, Context context, float gridSize, Player player) {
+        // Save Grid Size
+        this.gridSize = gridSize;
 
-		initSkyBox();
-		loadTextures(gl, context);
+        initSkyBox();
+        loadTextures(gl, context);
 
-		// Setup standard square index and tex buffers
-		// Define indices and tex coords
-		float t = gridSize / 240.0f;
-		byte indices[] = { 0, 1, 3, 0, 3, 2 };
-		float texCoords[] = { 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f };
+        // Setup standard square index and tex buffers
+        // Define indices and tex coords
+        float t = gridSize / 240.0f;
+        byte indices[] = {0, 1, 3, 0, 3, 2};
+        float texCoords[] = {1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f};
 
-		float l = gridSize / 4;
-		t = l / 12;
-		float florTexCoords[] = { 0.0f, 0.0f, t, 0.0f, 0.0f, t, t, t };
+        float l = gridSize / 4;
+        t = l / 12;
+        float florTexCoords[] = {0.0f, 0.0f, t, 0.0f, 0.0f, t, t, t};
 
-		floorTexBuffer = GraphicUtils.convToFloatBuffer(florTexCoords);
-		textureBuffer = GraphicUtils.convToFloatBuffer(texCoords);
-		indexBuffer = GraphicUtils.convToByteBuffer(indices);
-		nIndices = indices.length;
+        floorTexBuffer = GraphicUtils.convToFloatBuffer(florTexCoords);
+        textureBuffer = GraphicUtils.convToFloatBuffer(texCoords);
+        indexBuffer = GraphicUtils.convToByteBuffer(indices);
+        nIndices = indices.length;
 
-	}
+        this.player = player;
 
-	private void loadTextures(GL10 gl, Context context) {
-		sbTopTexture = new GLTexture(gl, context, R.drawable.sb_top);
-		sbBottomTexture = new GLTexture(gl, context, R.drawable.sb_bottom);
-		sbLeftTexture = new GLTexture(gl, context, R.drawable.sb_left);
-		sbRightTexture = new GLTexture(gl, context, R.drawable.sb_right);
-		sbFrontTexture = new GLTexture(gl, context, R.drawable.sb_front);
-		sbBackTexture = new GLTexture(gl, context, R.drawable.sb_back);
-		floorTexture = new GLTexture(gl, context, R.drawable.floor);
-	}
+    }
 
-	private void initSkyBox() {
-		float d = (float) gridSize;
+    private void loadTextures(GL10 gl, Context context) {
+        sbTopTexture = new GLTexture(gl, context, R.drawable.sb_top);
+        sbBottomTexture = new GLTexture(gl, context, R.drawable.sb_bottom);
+        sbLeftTexture = new GLTexture(gl, context, R.drawable.sb_left);
+        sbRightTexture = new GLTexture(gl, context, R.drawable.sb_right);
+        sbFrontTexture = new GLTexture(gl, context, R.drawable.sb_front);
+        sbBackTexture = new GLTexture(gl, context, R.drawable.sb_back);
+        floorTexture = new GLTexture(gl, context, R.drawable.floor);
+    }
 
-		float sides[][] = { { d, -d, d, d, d, d, d, -d, -d, d, d, -d }, /* front */
-		{ -d, -d, d, -d, d, d, d, -d, d, d, d, d }, /* top */
-		{ d, d, d, -d, d, d, d, d, -d, -d, d, -d }, /* left */
-		{ -d, -d, d, d, -d, d, -d, -d, -d, d, -d, -d }, /* right */
-		{ d, -d, -d, d, d, -d, -d, -d, -d, -d, d, -d }, /* bottom */
-		{ -d, d, d, -d, -d, d, -d, d, -d, -d, -d, -d } };/* back */
+    private void initSkyBox() {
+        float d = gridSize;
 
-		for (int i = 0; i < 6; i++) {
-			skyboxVertexBuffers[i] = GraphicUtils.convToFloatBuffer(sides[i]);
-		}
-	}
+        float sides[][] = { {d, -d, d, d, d, d, d, -d, -d, d, d, -d}, /* front */
+        {-d, -d, d, -d, d, d, d, -d, d, d, d, d}, /* top */
+        {d, d, d, -d, d, d, d, d, -d, -d, d, -d}, /* left */
+        {-d, -d, d, d, -d, d, -d, -d, -d, d, -d, -d}, /* right */
+        {d, -d, -d, d, d, -d, -d, -d, -d, -d, d, -d}, /* bottom */
+        {-d, d, d, -d, -d, d, -d, d, -d, -d, -d, -d}};/* back */
 
-	public void drawFloorTextured(GL10 gl) {
-		int i, j, l;
+        for (int i = 0; i < 6; i++) {
+            skyboxVertexBuffers[i] = GraphicUtils.convToFloatBuffer(sides[i]);
+        }
+    }
 
-		gl.glEnable(GL10.GL_BLEND);
-		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
-		gl.glEnable(GL10.GL_TEXTURE_2D);
-		gl.glBindTexture(GL10.GL_TEXTURE_2D, floorTexture.getTextureID());
+    public void drawFloorTextured(GL10 gl) {
+        int i, j, l;
 
-		gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        gl.glEnable(GL10.GL_BLEND);
+        gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+        gl.glEnable(GL10.GL_TEXTURE_2D);
+        gl.glBindTexture(GL10.GL_TEXTURE_2D, floorTexture.getTextureID());
 
-		l = (int) (gridSize / 4);
+        gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 
-		for (i = 0; i < (int) gridSize; i += l) {
-			for (j = 0; j < (int) gridSize; j += l) {
-				float rawVertices[] = new float[4 * 3];
+        l = (int) (gridSize / 4);
 
-				rawVertices[0] = (float) i;
-				rawVertices[1] = (float) j;
-				rawVertices[2] = 0.0f;
+        for (i = 0; i < (int) gridSize; i += l) {
+            for (j = 0; j < (int) gridSize; j += l) {
+                float rawVertices[] = new float[4 * 3];
 
-				rawVertices[3] = (float) (i + l);
-				rawVertices[4] = (float) j;
-				rawVertices[5] = 0.0f;
+                rawVertices[0] = i;
+                rawVertices[1] = j;
+                rawVertices[2] = 0.0f;
 
-				rawVertices[6] = (float) i;
-				rawVertices[7] = (float) (j + l);
-				rawVertices[8] = 0.0f;
+                rawVertices[3] = i + l;
+                rawVertices[4] = j;
+                rawVertices[5] = 0.0f;
 
-				rawVertices[9] = (float) (i + l);
-				rawVertices[10] = (float) (j + l);
-				rawVertices[11] = 0.0f;
+                rawVertices[6] = i;
+                rawVertices[7] = j + l;
+                rawVertices[8] = 0.0f;
 
-				FloatBuffer VertexBuffer = GraphicUtils
-						.convToFloatBuffer(rawVertices);
+                rawVertices[9] = i + l;
+                rawVertices[10] = j + l;
+                rawVertices[11] = 0.0f;
 
-				gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-				gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+                FloatBuffer VertexBuffer = GraphicUtils.convToFloatBuffer(rawVertices);
 
-				gl.glVertexPointer(3, GL10.GL_FLOAT, 0, VertexBuffer);
-				gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, floorTexBuffer);
+                gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+                gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
 
-				// Draw the vertices as triangles, based on the Index Buffer
-				// information
-				gl.glDrawElements(GL10.GL_TRIANGLE_STRIP, nIndices,
-						GL10.GL_UNSIGNED_BYTE, indexBuffer);
+                gl.glVertexPointer(3, GL10.GL_FLOAT, 0, VertexBuffer);
+                gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, floorTexBuffer);
 
-				// Disable the client state before leaving
-				gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
-				gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
-			}
-		}
-	}
+                // Draw the vertices as triangles, based on the Index Buffer
+                // information
+                gl.glDrawElements(GL10.GL_TRIANGLE_STRIP, nIndices, GL10.GL_UNSIGNED_BYTE, indexBuffer);
 
-	public void drawSkyBox(GL10 gl) {
-		gl.glEnable(GL10.GL_TEXTURE_2D);
-		gl.glDepthMask(false);
-		gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+                // Disable the client state before leaving
+                gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+                gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+            }
+        }
+    }
 
-		for (int i = 0; i < 6; i++) {
-			int textureToBind;
+    public void drawSkyBox(GL10 gl) {
+        gl.glEnable(GL10.GL_TEXTURE_2D);
+        gl.glDepthMask(false);
+        gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 
-			switch (i) {
-			case 0:
-				textureToBind = sbFrontTexture.getTextureID();
-				break;
-			case 1:
-				textureToBind = sbTopTexture.getTextureID();
-				break;
-			case 2:
-				textureToBind = sbLeftTexture.getTextureID();
-				break;
-			case 3:
-				textureToBind = sbRightTexture.getTextureID();
-				break;
-			case 4:
-				textureToBind = sbBottomTexture.getTextureID();
-				break;
-			default:
-				textureToBind = sbBackTexture.getTextureID();
-			}
-			gl.glBindTexture(GL10.GL_TEXTURE_2D, textureToBind);
+        for (int i = 0; i < 6; i++) {
+            int textureToBind;
 
-			gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-			gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
-			gl.glFrontFace(GL10.GL_CCW);
+            switch (i) {
+                case 0:
+                    textureToBind = sbFrontTexture.getTextureID();
+                    break;
+                case 1:
+                    textureToBind = sbTopTexture.getTextureID();
+                    break;
+                case 2:
+                    textureToBind = sbLeftTexture.getTextureID();
+                    break;
+                case 3:
+                    textureToBind = sbRightTexture.getTextureID();
+                    break;
+                case 4:
+                    textureToBind = sbBottomTexture.getTextureID();
+                    break;
+                default:
+                    textureToBind = sbBackTexture.getTextureID();
+            }
+            gl.glBindTexture(GL10.GL_TEXTURE_2D, textureToBind);
 
-			gl.glVertexPointer(3, GL10.GL_FLOAT, 0, skyboxVertexBuffers[i]);
-			gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, textureBuffer);
+            gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+            gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+            gl.glFrontFace(GL10.GL_CCW);
 
-			gl.glDisableClientState(GL10.GL_LIGHTING);
-			gl.glDrawElements(GL10.GL_TRIANGLES, nIndices,
-					GL10.GL_UNSIGNED_BYTE, indexBuffer);
-			gl.glEnableClientState(GL10.GL_LIGHTING);
-			gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
-			gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+            gl.glVertexPointer(3, GL10.GL_FLOAT, 0, skyboxVertexBuffers[i]);
+            gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, textureBuffer);
 
-		}
-		gl.glDisable(GL10.GL_TEXTURE_2D);
-		gl.glDepthMask(true);
+            gl.glPushMatrix();
+            gl.glTranslatef(player.getXCoord(), player.getYCoord(), 0);
 
-	}
+            gl.glDisableClientState(GL10.GL_LIGHTING);
+            gl.glDrawElements(GL10.GL_TRIANGLES, nIndices, GL10.GL_UNSIGNED_BYTE, indexBuffer);
+
+            gl.glPopMatrix();
+
+            gl.glEnableClientState(GL10.GL_LIGHTING);
+            gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+            gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+
+        }
+        gl.glDisable(GL10.GL_TEXTURE_2D);
+        gl.glDepthMask(true);
+
+    }
 
 }
